3f71f8a67c74356f908b02ca97b7c10717652d3b
Pablo Valdes <pablovaldes0925@gmail.com>
2024-08-16 22:52:15 -0400

    weather map api blah blah

--- react-basics/react-practice/src/Home.jsx
+++ react-basics/react-practice/src/Home.jsx
@@ -8,6 +8,11 @@ const Home = () => {
     const [things, setThings] = useState([])
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState(null)
+    const [weatherData, setWeatherData] = useState(null)
+    const [location, setLocation] = useState('')
+
+    const openWeatherMapKey = import.meta.env.VITE_OPEN_WEATHER_MAP_KEY
+
 
     useEffect(() => {
         const fetchThings = async () => {
@@ -20,23 +25,66 @@ const Home = () => {
                 setError(err)
             }
         }
+
         fetchThings()
     }, [])
+
+    const getWeatherData = async () => {
+        try {
+            const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${openWeatherMapKey}`)
+            const jsonRes = await response.json()
+            console.log(jsonRes)
+            setWeatherData(jsonRes)
+        } catch (err) {
+            console.error("Error fetching weather", err)
+        }
+    }
+
+    const handleSearch = () => {
+        if (location) {
+            getWeatherData()
+        } else {
+            alert("Please enter a location")
+        }
+    }
 
     if (loading) return <h2>Loading...</h2>
     if (error) return <h2>{error}</h2>
 
     return (
         <div>
-            { things.map((thing) => (
+            {things.map((thing) => (
                 <div key={thing.id}>
                     <Post id={thing.id} title={thing.title} things={things} setThings={setThings} />
                 </div>
-            )) }
+            ))}
             <PostForm things={things} setThings={setThings} />
+            <h1>Weather</h1>
+            <input
+                onChange={(e) => setLocation(e.target.value)}
+                type='text'
+                placeholder='enter location'
+                value={location}
+            />
+            <button onClick={handleSearch}>Get weather</button>
+            <div>
+                {weatherData && (
+                    <pre>{JSON.stringify(weatherData, null, 2)}</pre>
+                )}
+            </div>
+            {weatherData && weatherData.weather && (
+                <div>
+                    <h2>Weather Details</h2>
+                    {weatherData.weather.map((data, index) => (
+                        <div key={index}>
+                            <p>Main: {data.main}</p>
+                            <p>Description: {data.description}</p>
+                        </div>
+                    ))}
+                </div>
+            )}
         </div>
     )
-
 }
 
 export default Home
